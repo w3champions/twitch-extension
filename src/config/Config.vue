1924b3a5bab0f70ef8e9ec58f3515ec17989d126
Sergey <sergeykhval@gmail.com>
2021-01-10 22:29:42 +0100

    improve config page

--- src/config/Config.vue
+++ src/config/Config.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="config-page">
+    <p class="config-page__disclaimer">
+      This extension only supports 1v1 matches.
+    </p>
+    <p v-if="battleTagFromConfig" class="config-page__saved-battletag">
+      Your saved battle tag is: <strong>{{ battleTagFromConfig }}</strong>
+    </p>
     <p>
       Please provide your BattleTag exactly as it appears on your battle.net
       account page (it is case sensitive).
@@ -8,8 +14,8 @@
       class="config-page__img"
       src="../assets/battletag.png"
       alt="battle tag"
-      width="861"
-      height="180"
+      width="568"
+      height="325"
     />
     <form @submit.prevent="saveSettings" class="config-page__form">
       <div>
@@ -19,12 +25,14 @@
             type="text"
             id="battleTag"
             v-model="battleTag"
+            pattern="(^([A-zÀ-ú][A-zÀ-ú0-9]{2,11})|(^([а-яёА-ЯЁÀ-ú][а-яёА-ЯЁ0-9À-ú]{2,11})))(#[0-9]{4,})$"
             placeholder="BattleTag#1234"
+            required
           />
         </label>
       </div>
       <div>
-        <button type="submit">Save config</button>
+        <button type="submit">Save</button>
       </div>
       <p v-if="saved" style="color: green">Battletag saved!</p>
     </form>
@@ -32,44 +40,72 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import { Segment } from "@/typings";
-
-interface ComponentData {
-  battleTag: string;
-  saved: boolean;
-}
 
 export default defineComponent({
   name: "Config",
-  data(): ComponentData {
-    return {
-      battleTag: "",
-      saved: false
-    };
-  },
-  methods: {
-    saveSettings() {
+  setup() {
+    const battleTagFromConfig = ref("");
+    const battleTag = ref("");
+    const saved = ref(false);
+
+    function saveSettings() {
       const content = JSON.stringify({
-        battleTag: this.battleTag
+        battleTag: battleTag.value
       });
       window.Twitch.ext.configuration.set(Segment.Broadcaster, "", content);
 
-      this.saved = true;
+      saved.value = true;
+      battleTagFromConfig.value = battleTag.value;
 
       setTimeout(() => {
-        this.saved = false;
+        saved.value = false;
       }, 4000);
     }
+
+    onMounted(() => {
+      window.Twitch.ext.configuration.onChanged(() => {
+        if (window.Twitch.ext.configuration.broadcaster) {
+          const config = JSON.parse(
+            window.Twitch.ext.configuration.broadcaster?.content
+          );
+
+          battleTagFromConfig.value = config.battleTag;
+        }
+      });
+    });
+
+    return {
+      battleTagFromConfig,
+      battleTag,
+      saved,
+      saveSettings
+    };
   }
 });
 </script>
 
 <style scoped lang="scss">
 .config-page {
+  &__disclaimer {
+    background: #fb755d;
+    padding: 10px;
+    font-size: 18px;
+    border-radius: 8px;
+  }
+
+  &__saved-battletag {
+    font-size: 20px;
+    margin-bottom: 25px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #83817e;
+  }
+
   &__img {
     margin-bottom: 25px;
   }
+
   &__form {
     background: white;
     display: grid;
